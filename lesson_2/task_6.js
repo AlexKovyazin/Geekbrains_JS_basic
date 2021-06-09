815de582260776a36ba389f1beceaa8867f13dbc
AlexKovyazin <76786127+AlexKovyazin@users.noreply.github.com>
2021-06-09 17:25:49 +0300

    Update task_6.js

--- lesson_2/task_6.js
+++ lesson_2/task_6.js
@@ -28,13 +28,10 @@ function mathOperation(arg1, arg2, operation) {
     switch (operation.toLowerCase()) {
         case 'add':
             return add(arg1, arg2);
-            break;
         case 'sub':
             return sub(arg1, arg2);
-            break;
         case 'mul':
             return mul(arg1, arg2);
-            break;
         case 'div':
             return div(arg1, arg2);
     }
